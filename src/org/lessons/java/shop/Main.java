c652cdf07b5d6e5ef36840e41504303262cf8838
LorisMarzocchi <ml77182a@gmail.com>
2023-10-20 16:39:54 +0200

    oop shop

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,2 +1,41 @@
-package org.lessons.java.shop;public class Main {
+package org.lessons.java.shop;
+
+public class Main {
+    public static void main(String[] args) {
+    //  PRODOTTO 1
+        Prodotto prodotto1 = new Prodotto("Prodotto1", "loremadsd", 16.9,20);
+//        System.out.println("codice: " + prodotto1.getCodice() + " - " +
+//                            "nome: " + prodotto1.getNome() + " - " +
+//                            "descrizione: " + prodotto1.getDescrizione() + " - " +
+//                            "prezzo con iva: " + prodotto1.getPrezzoConIva() + " - " +
+//                            "prezzo: " + prodotto1.getPrezzo() + " - " +
+//                            "iva: " + prodotto1.getIva()
+//        );
+
+    //  PRODOTTO 2
+        Prodotto prodotto2 = new Prodotto("Prodotto2", "loremadsd", 19.9,10);
+//        System.out.println("codice: " + prodotto2.getCodice() + " - " +
+//                            "nome: " + prodotto2.getNome() + " - " +
+//                            "descrizione: " + prodotto2.getDescrizione() + " - " +
+//                            "prezzo con iva: " + prodotto2.getPrezzoConIva() + " - " +
+//                            "prezzo: " + prodotto2.getPrezzo() + " - " +
+//                            "iva: " + prodotto2.getIva()
+//        );
+
+    //  PRODOTTO 3
+        Prodotto prodotto3 = new Prodotto("prodotto3", "loremadsd", 15.8,15);
+//        System.out.println("codice: " + prodotto3.getCodice() + " - " +
+//                "nome: " + prodotto3.getNome() + " - " +
+//                "descrizione: " + prodotto3.getDescrizione() + " - " +
+//                "prezzo con iva: " + prodotto3.getPrezzoConIva() + " - " +
+//                "prezzo: " + prodotto3.getPrezzo() + " - " +
+//                "iva: " + prodotto3.getIva()
+//        );
+
+        System.out.println(prodotto1.getNomeCompleto());
+        System.out.println(prodotto2.getNomeCompleto());
+        System.out.println(prodotto3.getNomeCompleto());
+
+
+    }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,66 @@
-package org.lessons.java;
+package org.lessons.java.shop;
+
+import java.text.DecimalFormat;
+import java.util.Random;
 
 public class Prodotto {
 
+    private final int codice = new Random().nextInt(99999999);
+    private String nome;
+    private String descrizione;
+    private double prezzo;
+    private double iva;
+
+    public Prodotto( String nome, String descrizione, double prezzo, double iva) {
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    public String getPaddedCodice() {
+        return String.format("%08d", codice);
+    }
+
+
+    public String getNomeCompleto(){
+        return "Codice: " + getPaddedCodice() + " - Nome: " + nome + " - Prezzo: " + getPrezzoConIva();
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public double getPrezzoBase(){
+        return prezzo;
+    }
+
+    public double getPrezzoConIva() {
+        DecimalFormat df = new DecimalFormat("#.00");
+        double prezzoIva = prezzo + (prezzo * iva / 100);
+        return Double.parseDouble(df.format(prezzoIva).replace(',', '.'));
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
 }
